82cadacf7a2305e04e2704e1baa7e999ece47b4b
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-10-27 16:53:35 -0500

    Write Rating class

--- model.py
+++ model.py
@@ -40,6 +40,25 @@ class Movie(db.Model):
         return f'<Movie movie_id={self.movie_id} title={self.title}>'
 
 
+class Rating(db.Model):
+    """A rating."""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    movie = db.relationship("Movie", back_populates="ratings")
+    user = db.relationship("User", back_populates="ratings")
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}'
+
+
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
